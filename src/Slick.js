1a1e843491ab7ddba44838f7d3187271ec77fcd2
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-04-06 14:02:13 +0900

    up

--- src/Slick.js
+++ src/Slick.js
@@ -31,39 +31,7 @@ export default class Slick extends Component {
             <h2 class="slider__greet">
              Ryosuke Kobayashi
             </h2>
-            <img src="photo2.jpg"/>
-            <div class="anime-line"></div>
-          </div>
-
-          <div class="slider">
-            <div class="slider__header">
-              <ul>
-                <li><a href="#top">Top</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#works">Works</a></li>
-              </ul>
-            </div>
-            <div class="slider__box">
-              <h2 class="slider__box__greet">
-                Ryosuke Kobayashi
-              </h2>
-            </div>
-            <img src="photo3.jpg"/>
-            <div class="anime-line"></div>
-          </div>
-
-          <div class="slider">
-            <div class="slider__header">
-              <ul>
-                <li><a href="#top">Top</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#works">Works</a></li>
-              </ul>
-            </div>
-            <h2 class="slider__greet">
-             Ryosuke Kobayashi
-            </h2>
-            <img src="photo4.jpeg"/>
+            <img src="photo-1.jpg"/>
             <div class="anime-line"></div>
           </div>
 
@@ -78,7 +46,7 @@ export default class Slick extends Component {
             <h2 class="slider__greet">
               Ryosuke Kobayashi
             </h2>
-            <img src="photo5.jpeg"/>
+            <img src="photo-2.jpg"/>
             <div class="anime-line"></div>
           </div>
 
@@ -93,7 +61,52 @@ export default class Slick extends Component {
             <h2 class="slider__greet">
              Ryosuke Kobayashi
             </h2>
-            <img src="photo6.jpeg"/>
+            <img src="photo-3.jpg"/>
+            <div class="anime-line"></div>
+          </div>
+
+          <div class="slider">
+            <div class="slider__header">
+              <ul>
+                <li><a href="#top">Top</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#works">Works</a></li>
+              </ul>
+            </div>
+            <h2 class="slider__greet">
+              Ryosuke Kobayashi
+            </h2>
+            <img src="photo-4.jpg"/>
+            <div class="anime-line"></div>
+          </div>
+
+          <div class="slider">
+            <div class="slider__header">
+              <ul>
+                <li><a href="#top">Top</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#works">Works</a></li>
+              </ul>
+            </div>
+            <h2 class="slider__greet">
+             Ryosuke Kobayashi
+            </h2>
+            <img src="photo-5.jpg"/>
+            <div class="anime-line"></div>
+          </div>
+
+          <div class="slider">
+            <div class="slider__header">
+              <ul>
+                <li><a href="#top">Top</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#works">Works</a></li>
+              </ul>
+            </div>
+            <h2 class="slider__greet">
+             Ryosuke Kobayashi
+            </h2>
+            <img src="photo-6.jpg"/>
             <div class="anime-line"></div>
           </div>
         </Slider>
